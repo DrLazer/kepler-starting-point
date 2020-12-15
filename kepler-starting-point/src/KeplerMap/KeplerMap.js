e455c0879ba4832dc6e26ddef2585cee59689c00
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2020-12-15 10:45:54 +0000

    add sass and styles

--- kepler-starting-point/src/KeplerMap/KeplerMap.js
+++ kepler-starting-point/src/KeplerMap/KeplerMap.js
@@ -3,18 +3,20 @@ import KeplerGl from 'kepler.gl';
 import Settings from '../Settings';
 import AutoSizer from "react-virtualized/dist/commonjs/AutoSizer";
 
+import './kepler-map.scss';
+
 export default function KeplerMap(props) {
   
   return ( 
-    <div className="App" style={{'position':'absolute','width':'100%','height':'100%'}}>
+    <div className='kepler-map'>
       <AutoSizer>
         {({height, width}) => (
-        <KeplerGl
-          id="keplermap"
-          width={width}
-          height={height}
-          mapboxApiAccessToken={Settings.MAPBOX_KEY}
-        />
+          <KeplerGl
+            id="keplermap"
+            width={width}
+            height={height}
+            mapboxApiAccessToken={Settings.MAPBOX_KEY}
+          />
         )}
       </AutoSizer>
     </div>
